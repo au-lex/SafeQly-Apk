5ff9fd10b873cadb619c6ff0f129e4d63372652c
au-lex <aulexc4d2@gmail.com>
2025-12-03 00:47:41 +0100

    Refactor icon components and remove unused files

--- app/index.tsx
+++ app/index.tsx
@@ -1,7 +1,6 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
-import Login from '@/src/screens/auth/login/Login'
-import SafeLayout from '@/safeLayout/SafeLayout'
+
 import OnboardingSide from '@/src/screens/onboarding/Onboarding'
 
 const index = () => {
